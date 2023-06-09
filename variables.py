1b6db28c1a83af709b5dc857efeabb984d11fdb5
SantiagoAHS <lionelsanti2018@gmail.com>
2023-06-09 16:21:04 -0600

    02 variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Pyton es NO tipado
+
+name="Santiago"
+last_name="Arturo"
+age=19
+heigth=1.68
+is_student=True
+groups=["3A","3B","3C"]
+
+#funcion type
+print(type(name))
+print(type(age))
+print(type(heigth))
+print(type(is_student))
+print(type(groups))
