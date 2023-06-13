b2f573b03f4ea4a35a68887660b42de73fec6179
SantiagoAHS <lionelsanti2018@gmail.com>
2023-06-13 13:01:46 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,24 @@
+my_string = 'esTe eS Mi StrInG'
+
+#logitud de string 
+print(len(my_string))
+#mayusculas
+print(my_string.upper())
+#minisculas
+print(my_string.lower())
+#capitalice
+print(my_string.capitalize())
+#alinear espacios
+print(my_string.strip())
+
+#indexing and slicing
+my_string='hola 3C DSM'
+print(my_string[0])
+print(my_string[6])
+print(my_string[3])
+print(my_string[-1])
+print(my_string[-2])
+
+print(my_string[0:2])
+print(my_string[5:7])
+print(my_string[5:-1])
